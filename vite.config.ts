d34a9ee1b3fa6e6b9ecee9038191e95fbcdd45fd
xZeroW <victordmartins@protonmail.com>
2026-08-13 05:02:13 -0300

    Reduce initial framework bundle

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/asc-darkmoon-builder/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      react: 'preact/compat',
+      'react-dom': 'preact/compat',
+      'react-dom/client': 'preact/compat',
+    },
+  },
 })
